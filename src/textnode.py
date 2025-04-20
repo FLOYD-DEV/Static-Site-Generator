58a044b84bb24ff61d5e23a1be8a5094ff517adf
FLOYD-DEV <Christopher.Welton@proton.me>
2025-04-20 12:37:26 -0700

    Add markdown_to_blocks with tests

--- src/textnode.py
+++ src/textnode.py
@@ -106,3 +106,7 @@ def text_to_textnodes(text):
     nodes = split_nodes_image(nodes)
     nodes = split_nodes_link(nodes)
     return nodes
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    cleaned_blocks = [block.strip() for block in blocks if block.strip()]
+    return cleaned_blocks
